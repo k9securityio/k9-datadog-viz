01f484764275c008b9a105709ef862c977c93eaf
Stephen Kuenzli <skuenzli@k9security.io>
2023-01-17 16:36:58 -0700

    Map analysis_time property to event date after parsing each event.

--- pipelines/k9-reports-latest.pipeline.py
+++ pipelines/k9-reports-latest.pipeline.py
@@ -15,11 +15,6 @@
                  'source': 'aws.s3.key',
                  'type': 'grok-parser'},
                 {'is_enabled': True,
-                 'name': 'Set analysis_time k9 report attribute to official '
-                         'timestamp',
-                 'sources': ['analysis_time'],
-                 'type': 'date-remapper'},
-                {'is_enabled': True,
                  'is_replace_missing': True,
                  'name': 'Set service to k9',
                  'target': 'service',
@@ -37,15 +32,14 @@
                                           'support_rules': ''},
                                  'is_enabled': True,
                                  'name': 'Parse principal access summary',
-                                 'samples': [
-                                     '2022-12-21T07:15:36.947776+00:00,ci,arn:aws:iam::139710491120:user/ci,IAMUser,"{""BusinessUnit"": '
-                                     '""ProductDevelopment""}",Athena,administer-resource,',
-                                     '2022-12-21T07:15:36.947776+00:00,ci,arn:aws:iam::139710491120:user/ci,IAMUser,"{""BusinessUnit"": '
-                                     '""ProductDevelopment""}",DynamoDB '
-                                     'Accelerator '
-                                     '(DAX),administer-resource,\n',
-                                     '2022-12-21T07:15:36.947776+00:00,ci,arn:aws:iam::139710491120:user/ci,IAMUser,"{""BusinessUnit"": '
-                                     '""ProductDevelopment""}",STS,use-resource,arn:aws:iam::139710491120:role/DatadogIntegrationRole'],
+                                 'samples': ['2022-12-21T07:15:36.947776+00:00,ci,arn:aws:iam::139710491120:user/ci,IAMUser,"{""BusinessUnit"": '
+                                             '""ProductDevelopment""}",Athena,administer-resource,',
+                                             '2022-12-21T07:15:36.947776+00:00,ci,arn:aws:iam::139710491120:user/ci,IAMUser,"{""BusinessUnit"": '
+                                             '""ProductDevelopment""}",DynamoDB '
+                                             'Accelerator '
+                                             '(DAX),administer-resource,\n',
+                                             '2022-12-21T07:15:36.947776+00:00,ci,arn:aws:iam::139710491120:user/ci,IAMUser,"{""BusinessUnit"": '
+                                             '""ProductDevelopment""}",STS,use-resource,arn:aws:iam::139710491120:role/DatadogIntegrationRole'],
                                  'source': 'message',
                                  'type': 'grok-parser'},
                                 {'grok': {'match_rules': 'principal_tag_rule '
@@ -56,7 +50,12 @@
                                  'samples': ['{"k9security:analysis": '
                                              '"include"}'],
                                  'source': 'principal_access_summary.principal_tags',
-                                 'type': 'grok-parser'}],
+                                 'type': 'grok-parser'},
+                                {'is_enabled': True,
+                                 'name': 'Set analysis_time k9 report '
+                                         'attribute to official timestamp',
+                                 'sources': ['principal_access_summary.analysis_time'],
+                                 'type': 'date-remapper'}],
                  'type': 'pipeline'},
                 {'filter': {'query': '@k9.report.type:"principals"'},
                  'is_enabled': True,
@@ -66,17 +65,16 @@
                                           'support_rules': ''},
                                  'is_enabled': True,
                                  'name': 'Parse principal summary',
-                                 'samples': [
-                                     '2022-12-10T07:14:19.753800+00:00,ci,arn:aws:iam::139710491120:user/ci,IAMUser,True,2022-12-10 '
-                                     '05:03:00+00:00,ProductDevelopment,,,"{""BusinessUnit"": '
-                                     '""ProductDevelopment""}",,,,2022-12-10 '
-                                     '05:03:00+00:00,2021-03-08 '
-                                     '21:03:09+00:00,Active,,,\n',
-                                     '2022-12-10T07:14:19.753800+00:00,AWSReservedSSO_AdministratorAccess_437be9d757c9ea2f,arn:aws:iam::139710491120:role/aws-reserved/sso.amazonaws.com/AWSReservedSSO_AdministratorAccess_437be9d757c9ea2f,IAMRole,True,2022-07-21 '
-                                     '15:30:39+00:00,,,,{},,,,,,,,,',
-                                     '2022-12-10T07:14:19.753800+00:00,datadog-ForwarderStack-1OV25NQJZ3PDG-ForwarderRole-1X48OL78SFSGB,arn:aws:iam::139710491120:role/datadog-ForwarderStack-1OV25NQJZ3PDG-ForwarderRole-1X48OL78SFSGB,IAMRole,False,2022-12-10 '
-                                     '05:24:59+00:00,,,,"{""dd_forwarder_version"": '
-                                     '""3.40.0""}",,,,,,,,,'],
+                                 'samples': ['2022-12-10T07:14:19.753800+00:00,ci,arn:aws:iam::139710491120:user/ci,IAMUser,True,2022-12-10 '
+                                             '05:03:00+00:00,ProductDevelopment,,,"{""BusinessUnit"": '
+                                             '""ProductDevelopment""}",,,,2022-12-10 '
+                                             '05:03:00+00:00,2021-03-08 '
+                                             '21:03:09+00:00,Active,,,\n',
+                                             '2022-12-10T07:14:19.753800+00:00,AWSReservedSSO_AdministratorAccess_437be9d757c9ea2f,arn:aws:iam::139710491120:role/aws-reserved/sso.amazonaws.com/AWSReservedSSO_AdministratorAccess_437be9d757c9ea2f,IAMRole,True,2022-07-21 '
+                                             '15:30:39+00:00,,,,{},,,,,,,,,',
+                                             '2022-12-10T07:14:19.753800+00:00,datadog-ForwarderStack-1OV25NQJZ3PDG-ForwarderRole-1X48OL78SFSGB,arn:aws:iam::139710491120:role/datadog-ForwarderStack-1OV25NQJZ3PDG-ForwarderRole-1X48OL78SFSGB,IAMRole,False,2022-12-10 '
+                                             '05:24:59+00:00,,,,"{""dd_forwarder_version"": '
+                                             '""3.40.0""}",,,,,,,,,'],
                                  'source': 'message',
                                  'type': 'grok-parser'},
                                 {'grok': {'match_rules': 'principal_tags_rule '
@@ -89,7 +87,12 @@
                                              '{"k9security:analysis": '
                                              '"include", "STAGE": "dev"}'],
                                  'source': 'principal_summary.principal_tags',
-                                 'type': 'grok-parser'}],
+                                 'type': 'grok-parser'},
+                                {'is_enabled': True,
+                                 'name': 'Set analysis_time k9 report '
+                                         'attribute to official timestamp',
+                                 'sources': ['principal_summary.analysis_time'],
+                                 'type': 'date-remapper'}],
                  'type': 'pipeline'},
                 {'filter': {'query': '@k9.report.type:"resource-access-summaries"'},
                  'is_enabled': True,
@@ -99,12 +102,16 @@
                                           'support_rules': ''},
                                  'is_enabled': True,
                                  'name': 'Parse resource access summary',
-                                 'samples': [
-                                     '2022-12-21T07:15:36.947776+00:00,DynamoDB,TerraformStateLock,arn:aws:dynamodb:us-east-1:139710491120:table/TerraformStateLock,administer-resource,IAMUser,ci,arn:aws:iam::139710491120:user/ci,',
-                                     '2022-12-21T07:15:36.947776+00:00,DynamoDB,aws-mp-metering-records-dev-skuenzli,arn:aws:dynamodb:us-east-1:139710491120:table/aws-mp-metering-records-dev-skuenzli,administer-resource,IAMUser,ci,arn:aws:iam::139710491120:user/ci,',
-                                     '2022-12-21T07:15:36.947776+00:00,KMS,alias/k9-cdk-integration-test,arn:aws:kms:us-east-1:139710491120:key/a71d41d3-d7f2-4829-9d0c-8d527e366a9a,administer-resource,IAMUser,ci,arn:aws:iam::139710491120:user/ci,'],
+                                 'samples': ['2022-12-21T07:15:36.947776+00:00,DynamoDB,TerraformStateLock,arn:aws:dynamodb:us-east-1:139710491120:table/TerraformStateLock,administer-resource,IAMUser,ci,arn:aws:iam::139710491120:user/ci,',
+                                             '2022-12-21T07:15:36.947776+00:00,DynamoDB,aws-mp-metering-records-dev-skuenzli,arn:aws:dynamodb:us-east-1:139710491120:table/aws-mp-metering-records-dev-skuenzli,administer-resource,IAMUser,ci,arn:aws:iam::139710491120:user/ci,',
+                                             '2022-12-21T07:15:36.947776+00:00,KMS,alias/k9-cdk-integration-test,arn:aws:kms:us-east-1:139710491120:key/a71d41d3-d7f2-4829-9d0c-8d527e366a9a,administer-resource,IAMUser,ci,arn:aws:iam::139710491120:user/ci,'],
                                  'source': 'message',
-                                 'type': 'grok-parser'}],
+                                 'type': 'grok-parser'},
+                                {'is_enabled': True,
+                                 'name': 'Set analysis_time k9 report '
+                                         'attribute to official timestamp',
+                                 'sources': ['resource_access_summary.analysis_time'],
+                                 'type': 'date-remapper'}],
                  'type': 'pipeline'},
                 {'filter': {'query': '@k9.report.type:"resources"'},
                  'is_enabled': True,
@@ -114,15 +121,14 @@
                                           'support_rules': ''},
                                  'is_enabled': True,
                                  'name': 'Parse resources',
-                                 'samples': [
-                                     '2023-01-13T13:14:19.753800+00:00,AccountAdminAccessRole-Sandbox,arn:aws:iam::139710491120:role/AccountAdminAccessRole-Sandbox,IAMRole,,,,,,,{}',
-                                     '2023-01-13T13:14:19.753800+00:00,qm-dev-k9-reports,arn:aws:s3:::qm-dev-k9-reports,S3Bucket,,dev,product,,,,"{\'Owner\': '
-                                     "'product', 'ManagedBy': "
-                                     "'Terraform', 'Environment': "
-                                     "'dev', 'Application': 'k9', "
-                                     "'Name': "
-                                     '\'qm-dev-k9-reports\'}"',
-                                     '2023-01-13T13:14:19.753800+00:00,int-test-pg-01,arn:aws:rds:us-east-1:139710491120:cluster:int-test-pg-01,RDSCluster,,,,,,,{}'],
+                                 'samples': ['2023-01-13T13:14:19.753800+00:00,AccountAdminAccessRole-Sandbox,arn:aws:iam::139710491120:role/AccountAdminAccessRole-Sandbox,IAMRole,,,,,,,{}',
+                                             '2023-01-13T13:14:19.753800+00:00,qm-dev-k9-reports,arn:aws:s3:::qm-dev-k9-reports,S3Bucket,,dev,product,,,,"{\'Owner\': '
+                                             "'product', 'ManagedBy': "
+                                             "'Terraform', 'Environment': "
+                                             "'dev', 'Application': 'k9', "
+                                             "'Name': "
+                                             '\'qm-dev-k9-reports\'}"',
+                                             '2023-01-13T13:14:19.753800+00:00,int-test-pg-01,arn:aws:rds:us-east-1:139710491120:cluster:int-test-pg-01,RDSCluster,,,,,,,{}'],
                                  'source': 'message',
                                  'type': 'grok-parser'},
                                 {'grok': {'match_rules': 'resource_tags_rule '
@@ -135,6 +141,11 @@
                                              '{"k9security:analysis": '
                                              '"include", "STAGE": "dev"}'],
                                  'source': 'resource_summary.resource_tags',
-                                 'type': 'grok-parser'}],
+                                 'type': 'grok-parser'},
+                                {'is_enabled': True,
+                                 'name': 'Set analysis_time k9 report '
+                                         'attribute to official timestamp',
+                                 'sources': ['resource_summary.analysis_time'],
+                                 'type': 'date-remapper'}],
                  'type': 'pipeline'}],
  'type': 'pipeline'}
