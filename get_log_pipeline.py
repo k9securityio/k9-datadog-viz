941b29f3b12a778922f053bd9c590963c8e3190d
Stephen Kuenzli <skuenzli@k9security.io>
2023-01-17 14:56:05 -0700

    Retrieve current k9-reports log processing pipeline

---
+++ get_log_pipeline.py
@@ -0,0 +1,17 @@
+"""
+Get a pipeline returns "OK" response
+"""
+
+from datadog_api_client import ApiClient, Configuration
+from datadog_api_client.v1.api.logs_pipelines_api import LogsPipelinesApi
+
+configuration = Configuration()
+with ApiClient(configuration) as api_client:
+    api_instance = LogsPipelinesApi(api_client)
+    # response = api_instance.list_logs_pipelines()
+    # print(response)
+    response = api_instance.get_logs_pipeline(
+        pipeline_id="-D4J40aCQLefllk8y9Rh9w",
+    )
+
+    print(response)
